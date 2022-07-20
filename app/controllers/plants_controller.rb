0ef898b97d2461972d0e5e93f45f36d2f5bf6a69
Ana Zitzow <anasempai89@gmail.com>
2022-07-20 15:45:44 -0700

    commit tree ab23ac794ec086ace4bacdbe99d53fa557bbd37f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  before_action :find_plant, only: [:show, :update, :destroy]
   # GET /plants
   def index
     plants = Plant.all
@@ -8,8 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: @plant
   end
 
   # POST /plants
@@ -18,7 +17,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # UPDATE /plants/:id
+  def update
+    @plant.update(plant_params)
+    render json: @plant, status: :ok
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    @plant.destroy
+
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    @plant = Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
